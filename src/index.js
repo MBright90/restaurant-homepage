7a3b4004f1a04e6b2f38d81f0e073b0b3d0a25f1
mbright90 <michael.bright90@icloud.com>
2022-08-30 13:44:45 +0100

    Create function to apply header and styles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 import './style.css'
+import GitIcon from './GitHub-Mark-Light-64px.png'
+import HomeImage from './main-food-photo.jpeg'  
 
 console.log('Do not worry, this is working')
+
+const createHeader = () => {
+    const header = document.createElement("header");
+    
+    const headerLogo = document.createElement("h1");
+    headerLogo.textContent = "Bellamy\'s";
+
+    const headerTabContainer = document.createElement("div");
+    headerTabContainer.classList.add("tab-links");
+
+    const listHolder = document.createElement("ul");
+
+    const tabOne = document.createElement("li");
+    const menuLink = document.createElement("a");
+    menuLink.textContent = "Menu";
+    tabOne.appendChild(menuLink);
+
+    const tabTwo = document.createElement("li");
+    const homepageLink = document.createElement("a");
+    homepageLink.textContent = "Homepage";
+    tabTwo.appendChild(homepageLink);
+
+    const tabThree = document.createElement("li");
+    const contactLink = document.createElement("a");
+    contactLink.textContent = "Contact Us";
+    tabThree.appendChild(contactLink);
+
+    listHolder.appendChild(tabOne);
+    listHolder.appendChild(tabTwo);
+    listHolder.appendChild(tabThree);
+
+    headerTabContainer.appendChild(listHolder);
+
+    header.appendChild(headerLogo);
+    header.appendChild(headerTabContainer);
+};
+
+const createFooter = () => {
+    const footer = document.createElement('Footer')
+}
+
+const createHomepage = () => {
+    const mainContainer = document.createElement("main")
+}
