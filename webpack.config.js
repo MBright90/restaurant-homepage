f274d3a8c1475d6704250e02b5a6d6d1cef816e1
mbright90 <michael.bright90@icloud.com>
2022-09-05 11:18:46 +0100

    Update config file

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const { ModuleFilenameHelpers } = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/index.js',
     plugins: [
         new HtmlWebpackPlugin({
